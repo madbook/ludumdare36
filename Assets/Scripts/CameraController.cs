8b83f2970051d1388d30632b3fc431d25f338c06
Matt Lee <matt@reddit.com>
2016-08-29 17:16:05 -0400

    Add extra camera rotation controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
     void Update () {
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			transform.RotateAround (Vector3.zero, Vector3.up, -45f);
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			transform.RotateAround (Vector3.zero, Vector3.up, 45f);
-		}
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            transform.RotateAround (Vector3.zero, Vector3.up, -45f);
+        } else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            transform.RotateAround (Vector3.zero, Vector3.up, 45f);
+        } else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+            transform.RotateAround (Vector3.zero, transform.right, 45);
+        } else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+            transform.RotateAround (Vector3.zero, transform.right, -45);
+        }
     }
 }
